f9cc12377bdc1bf2e1477c894831d91ae37052b1
Shelby Kira Marvell <shelby@marvell.cat>
2025-05-06 21:41:12 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>shelbyKiraM doing some silly?</title>
+</head>
+<body>
+<h1>Pretend this is an epic site</h1>
+<p>
+<?php // does PHP run like i want it to?
+echo 'hi'; ?>
+</p>
+</body>
+</html>
